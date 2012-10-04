9f3f4b6b8fe3c57710d29810582d21ba63e38f9d
jack.zhangyk@gmail.com <jack.zhangyk@gmail.com@0300133b-77a5-e19a-fdbc-4c7656fc2ffb>
2012-10-04 15:34:08 +0000

    将App信息移入到AppInfo中

---
+++ trunk/MyHistorySay/src/com/taobao/zhangjun/AppInfo.java
@@ -0,0 +1,7 @@
+package com.taobao.zhangjun;
+
+public class AppInfo {
+
+	public static String CONSUMER_KEY = "472609320";
+	public static String CONSUMER_SECRET = "0648fafd4c1414606121a50f2b83d22c";
+}
